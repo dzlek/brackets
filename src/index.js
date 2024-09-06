1a5c498099ae3c847669286ecc729c9257529bee
dzlek <6894255@gmail.com>
2024-09-06 20:48:06 +0300

    feat: add sltn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+  const openToClose = new Map();
+  const closeToOpen = new Map();
+
+  for (const [open, close] of bracketsConfig) {
+    openToClose.set(open, close);
+    closeToOpen.set(close, open);
+  }
+
+  const stack = [];
+
+  for (const char of str) {
+    if (openToClose.has(char)) {
+      stack.push(char);
+    } else if (closeToOpen.has(char)) {
+      if (
+        stack.length === 0 ||
+        stack[stack.length - 1] !== closeToOpen.get(char)
+      ) {
+        return false;
+      }
+
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
 }
+
+module.exports = check;
