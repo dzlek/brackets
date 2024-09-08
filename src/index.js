0208cc23dfb49fa9bd8955a41675a7edd545669d
dzlek <6894255@gmail.com>
2024-09-08 12:58:14 +0300

    fix: dowble brackets

--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ function check(str, bracketsConfig) {
   const openToClose = new Map();
   const closeToOpen = new Map();
 
+  const sameBrackets = new Set();
+
   for (const [open, close] of bracketsConfig) {
     openToClose.set(open, close);
     closeToOpen.set(close, open);
+    if (open === close) {
+      sameBrackets.add(open);
+    }
   }
 
   const stack = [];
 
   for (const char of str) {
-    if (openToClose.has(char)) {
+    if (sameBrackets.has(char)) {
+      if (stack.length > 0 && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (openToClose.has(char)) {
       stack.push(char);
     } else if (closeToOpen.has(char)) {
       if (
